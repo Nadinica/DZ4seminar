1778cded7fc48f2c1acfa981ad38b0407945f5ea
Nadin <icemylady@gmail.com>
2022-11-07 21:57:36 +0300

    Задача 25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B)
+// и возводит число A в целую степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+//A^B=A*A*A*A*A
+
+
+Console.WriteLine("Введите А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+if (b != 0)
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+Console.WriteLine(result);
