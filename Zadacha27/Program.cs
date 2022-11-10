80809ec4a010735b4a31cc5b12427a3de02c0781
Nadin <icemylady@gmail.com>
2022-11-10 13:24:53 +0300

    Задача 27

--- Zadacha27/Program.cs
+++ Zadacha27/Program.cs
@@ -9,40 +9,48 @@
 //3)суммировать числа массива;
 
 Console.WriteLine("Напишите число ");
-int Number = Convert.ToInt32(Console.ReadLine());
-int PoiskCount(int Number)
+int value = Convert.ToInt32(Console.ReadLine());
+int PoiskCount(int value)
 {
     int result=0;
-    while (Number!=0)
+    while (value!=0)
     {
         result+=1;
-        Number=Number/10;
+        value=value/10;
     }
     return result;
 }
-int count1 = PoiskCount(Number);
-Console.WriteLine(count1);
+int count1 = PoiskCount(value);
+//Console.WriteLine(count1);
 int [] Array(int value)  //2)Число разложить на массив;
 {
-    int count = PoiskCount(Number)-1;
+    int count = PoiskCount(value)-1;
     int[]arrey = new int [count+1];
     while (value!=0)
     {
-        arrey [count--]=Number%10;
-        Number=Number/10;
+        arrey [count--]=value%10;
+        value=value/10;
     }
     return arrey;
 }
-//Array(arrey);
 int Summa (int [] arrey) // 3)суммировать числа массива;
 {
-        int sum = 0;
+    int sum = 0;
     for (int i = 0; i < arrey.Length; i++)
     {
-        sum=sum+i;
+        sum=sum+arrey[i];
     }
     return sum;
 }
-int arr[]=Array(Number);
-int sum =Summa(arr);
-Console.WriteLine(sum);
+Console.Write("Сумма цифр в числе ");
+int [] arrey = Array (value);
+int size = arrey.Length;
+int index = 0;
+while (index < size)
+{
+   Console.Write(arrey[index++]);
+}
+
+int S = Summa (arrey);
+Console.Write(" равна ");
+Console.WriteLine(S);
